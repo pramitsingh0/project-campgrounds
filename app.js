631fe10975b570e73941ecb63ab509a8d9a83e07
pramitsingh0 <pramitsingh0@gmail.com>
2023-03-11 16:00:23 +0530

    implemented session

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const app = express();
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
+const session = require("express-session");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
 const campgroundRoutes = require("./routes/campgrounds");
@@ -11,21 +12,6 @@ const reviewRoutes = require("./routes/review");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-
-async function db() {
-  await mongoose.connect("mongodb://localhost:27017/project-campground");
-}
-
-mongoose.set("strictQuery", false);
-// Connect to DB
-db()
-  .then(() => {
-    console.log("Connected to db!");
-  })
-  .catch((e) => {
-    console.error("Connection to db error: ", e);
-  });
-
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -33,6 +19,27 @@ app.set("public", path.join(__dirname, "public"));
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/review", reviewRoutes);
 app.use(express.static("public"));
+const sessionConfig = {
+  secret: "somesecret",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  resave: false,
+  saveUninitialized: true,
+};
+app.use(session(sessionConfig));
+
+// DB CONNECTION
+async function db() {
+  await mongoose.connect("mongodb://localhost:27017/project-campground");
+}
+
+mongoose.set("strictQuery", false);
+db()
+  .then(() => {
+    console.log("Connected to db!");
+  })
+  .catch((e) => {
+    console.error("Connection to db error: ", e);
+  });
 
 // CREATE REVIEW
 
